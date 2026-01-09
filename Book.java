496f57753c10924e116ad15ed4c039d4075ea61d
muzughn <126944300+muzughn@users.noreply.github.com>
2026-01-09 22:42:19 +0800

    adding java activity 1

---
+++ Book.java
@@ -0,0 +1,9 @@
+package M2_Activity1;
+
+public class Book {
+	String title;
+	
+	public void read() {
+		System.out.println("Title: "+ title);
+	}
+}
---
+++ M2_Activity1.java
@@ -0,0 +1,14 @@
+package M2_Activity1;
+
+public class M2_Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book mybook = new Book();
+		mybook.title = "Harry Potter";
+		mybook.read();
+		
+		System.out.println();
+	}
+
+}
