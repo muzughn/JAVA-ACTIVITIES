40bd8ffed9972fc5ad92d4fa57f13ff618b581e5
muzughn <126944300+muzughn@users.noreply.github.com>
2026-01-09 16:35:55 +0800

    adding java activity 1

---
+++ M1_ACT1.java
@@ -0,0 +1,20 @@
+package M1_Activity1;
+
+import java.util.Scanner;
+
+public class M1_ACT1 {
+
+		public static void main(String[] args) {
+		
+			// Create a Scanner object
+			Scanner input = new Scanner(System.in);
+		
+			// Read user input
+			System.out.print("What is your name? ");
+			String Inpname = input.nextLine();
+		
+			//Display output 
+			System.out.print("Hello, " + Inpname + "!" );
+	}
+
+}
