f88ff304382a9832d16e87f9e683b2b17b9f6fc4
muzughn <126944300+muzughn@users.noreply.github.com>
2026-01-09 16:42:20 +0800

    adding java activity 2

---
+++ Seatwork2.java
@@ -0,0 +1,10 @@
+package M2_Activity2;
+
+public class Seatwork2 {
+    
+	public static void main(String[] args) {
+        Whiteboard wb = new Whiteboard();
+        int total = wb.addNumbers(5, 3);
+        System.out.println("The total is: " + String.valueOf(total));
+    }
+}
---
+++ Whiteboard.java
@@ -0,0 +1,13 @@
+package M2_Activity2;
+
+/**
+ * 
+ */
+public class Whiteboard {
+
+        int addNumbers(int addend1, int addend2) {
+        int sum = addend1 + addend2;
+        return sum;
+    }
+
+}
